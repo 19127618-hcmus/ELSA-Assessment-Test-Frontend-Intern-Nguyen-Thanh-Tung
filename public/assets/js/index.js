288f39e899a44f7942c19eb93a61f937ee0f3229
19127618 <91837148+19127618-hcmus@users.noreply.github.com>
2022-04-21 12:43:11 +0700

    responsive

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,7 +2,7 @@ let selectPrice = 0;
 
 function validatePhoneNumber(phoneNumber) {
     // var regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-    const regex = /(03|05|07|08|09|01[2|6|8|9])+([0-9]{8})\b/
+    const regex = /(03|05|07|08|09|01[2|6|8|9])+([0-9]{8})\b/;
     if (regex.test(phoneNumber)) {
         return true;
     }
@@ -10,7 +10,8 @@ function validatePhoneNumber(phoneNumber) {
 }
 
 function validateEmail(email) {
-    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const regex =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (regex.test(email)) {
         return true;
     }
@@ -18,7 +19,7 @@ function validateEmail(email) {
 }
 
 function checkNull(value) {
-    if (value == null || value == '') {
+    if (value == null || value == "") {
         return true;
     }
     return false;
@@ -31,61 +32,69 @@ $("#form-smBtn-content").click(function () {
     //check phone number
     if (!checkPhoneNumber) {
         $("#noti-phone").addClass("show");
-    }
-    else {
+    } else {
         $("#noti-phone").removeClass("show");
     }
 
     //check email
     if (!checkEmail) {
         $("#noti-email").addClass("show");
-    }
-    else {
+    } else {
         $("#noti-email").removeClass("show");
     }
 
     //check name
-    if (checkNull($("#name").val()) ) {
+    if (checkNull($("#name").val())) {
         $("#noti-null").addClass("show");
-    }
-    else {
+    } else {
         $("#noti-null").removeClass("show");
     }
 
     //check price
-    if(selectPrice == 0){
+    if (selectPrice == 0) {
         $("#noti-price").addClass("show");
-    }
-    else{
+    } else {
         $("#noti-price").removeClass("show");
-    }    
+    }
 
     //open modal
-    if (checkPhoneNumber && checkEmail && !checkNull($("#name").val()) && selectPrice == 1) {
+    if (
+        checkPhoneNumber &&
+        checkEmail &&
+        !checkNull($("#name").val()) &&
+        selectPrice == 1
+    ) {
         $(".modal-form").addClass("show");
     }
-}
-)
+});
 
 // close modal
 $(".close-btn").click(function () {
     $(".modal-form").removeClass("show");
-})
+});
 
 $(".modal-form-backgound").click(function () {
     $(".modal-form").removeClass("show");
-})
+});
 
 // price
-function showPrice(option){
-    if(option == 1) {
+function showPrice(option) {
+    if (option == 1) {
         $("#price1").addClass("show");
-        $("#price2").removeClass("show");}
-    else if(option == 2) {
+        $("#price2").removeClass("show");
+    } else if (option == 2) {
         $("#price2").addClass("show");
         $("#price1").removeClass("show");
     }
-    selectPrice=1;
+    selectPrice = 1;
     $("#noti-price").removeClass("show");
 }
+
+//reponsive
+$(window).resize(function () {
+    if ($(window).width() < 991) {
+        $(".section3-body-col").removeClass("col-md-3");
+        $(".section3-body-col").addClass("col-sm-6");
+    }
+});
 
